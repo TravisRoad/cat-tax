6f2213a0bcfcc07b6b145d4d811ec3138fecc80d
travis <lxymontage@gmail.com>
2025-01-16 13:47:39 +0800

    add ANALYTICS_ID

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,14 +38,14 @@ export default function RootLayout({
         <script
           async
           src="https://umami.lxythan2lxy.cn/script.js"
-          data-website-id="3eb5c5e9-a4ab-49f5-aa9e-7d47a4a55446"
+          data-website-id={process.env.NEXT_PUBLIC_ANALYTICS_ID_UMAMI}
         ></script>
         <Script id="clarity-script" strategy="afterInteractive">
           {`(function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "puh05hpeg3");`}
+    })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_ANALYTICS_ID_CLARITY}");`}
         </Script>
       </head>
       <body className={clsx("antialiased", "md:max-w-3xl mx-auto")}>
